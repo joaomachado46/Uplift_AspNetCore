b43e004d15d3c6894725bab2404fb5cdbc3f4d5a
joaomachado46 <joaopcmachado46@gmail.com>
2021-03-04 18:05:11 +0000

    Add project files.

---
+++ Uplift.DataModels/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Uplift_AspNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
